e0e92a19fe04354bab93980873cbc9cf92a62ab4
Darkborderman <reastw1234@gmail.com>
2023-08-13 02:24:28 +0800

    refactor: Comment test code

--- Contents/mods/HousecleaningArchitect/media/lua/client/ISUI/testscript.lua
+++ Contents/mods/HousecleaningArchitect/media/lua/client/ISUI/testscript.lua
@@ -1,19 +1,19 @@
 --Scripts for testing
 
-require('luautils');
+-- require('luautils');
 
-local square = getPlayer():getSquare()
+-- local square = getPlayer():getSquare()
 
-for i = 0,square:getObjects():size()-1 do
-  local object = square:getObjects():get(i);
-  if object then
-    local attached = object:getAttachedAnimSprite()
-    for n=1,attached:size() do
-      local sprite = attached:get(n-1)
-      print(sprite:getParentSprite():getName())
-    end
-  end
-end
+-- for i = 0,square:getObjects():size()-1 do
+--   local object = square:getObjects():get(i);
+--   if object then
+--     local attached = object:getAttachedAnimSprite()
+--     for n=1,attached:size() do
+--       local sprite = attached:get(n-1)
+--       print(sprite:getParentSprite():getName())
+--     end
+--   end
+-- end
 
 
 --[[
