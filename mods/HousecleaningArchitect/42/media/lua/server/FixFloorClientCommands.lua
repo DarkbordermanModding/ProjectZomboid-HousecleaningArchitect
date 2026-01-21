a0ff8e7f6c6732193d420dbde073463453f7de7c
Darkborderman <reastw1234@gmail.com>
2026-01-21 16:29:10 +0800

    refactor: Update

--- mods/HousecleaningArchitect/42/media/lua/server/FixFloorClientCommands.lua
+++ mods/HousecleaningArchitect/42/media/lua/server/FixFloorClientCommands.lua
@@ -1,37 +1,45 @@
 require('luautils');
 
-local function onFixFloorCommand(module, command, player, args)
-  if module == 'FixFloor' then
-    if command == 'FixFloorCommand' then
-      local sq = getCell():getGridSquare(args.x, args.y, args.z)
+local function cleanSquare(x, y, z)
+  local sq = getCell():getGridSquare(x, y, z)
+  if not sq then return end
 
-      if not sq then return end
-
-      for i=0,sq:getObjects():size()-1 do
-        local object = sq:getObjects():get(i);
-
-        if object then
-          local attached = object:getAttachedAnimSprite()
-          if attached then
-            for n = attached:size(), 1, -1 do
-              local sprite = attached:get(n-1)
-              if sprite and sprite:getParentSprite() and sprite:getParentSprite():getName() and
-                (
-                  luautils.stringStarts(sprite:getParentSprite():getName(), "floors_overlay_tiles") or
-                  luautils.stringStarts(sprite:getParentSprite():getName(), "floors_overlay_wood") or
-                  luautils.stringStarts(sprite:getParentSprite():getName(), "d_streetcrack") or
-                  luautils.stringStarts(sprite:getParentSprite():getName(), "floors_overlay_street") or
-                  luautils.stringStarts(sprite:getParentSprite():getName(), "d_wallcrack") or
-                  luautils.stringStarts(sprite:getParentSprite():getName(), "blends_streetoverlays")
-              ) then
-                object:RemoveAttachedAnim(n-1)
-                object:transmitUpdatedSpriteToClients()
-              end
+  local objects = sq:getObjects()
+  for i = 0, objects:size() - 1 do
+    local object = objects:get(i)
+    if object then
+      local attached = object:getAttachedAnimSprite()
+      if attached then
+        for n = attached:size() - 1, 0, -1 do
+          local sprite = attached:get(n)
+          if sprite then
+            local parent = sprite:getParentSprite()
+            local name = parent and parent:getName()
+            if name and (
+              luautils.stringStarts(name, "floors_overlay_tiles") or
+              luautils.stringStarts(name, "floors_overlay_wood") or
+              luautils.stringStarts(name, "d_streetcrack") or
+              luautils.stringStarts(name, "floors_overlay_street") or
+              luautils.stringStarts(name, "d_wallcrack") or
+              luautils.stringStarts(name, "blends_streetoverlays")
+            ) then
+              object:RemoveAttachedAnim(n)
+              object:transmitUpdatedSpriteToClients()
             end
           end
         end
       end
     end
+  end
+end
+
+local function onFixFloorCommand(module, command, player, args)
+  if module ~= 'FixFloor' or command ~= 'FixFloorCommand' then
+    return
+  end
+
+  for z = args.z, args.z + 5 do
+    cleanSquare(args.x, args.y, z)
   end
 end
 
